cd36820ef885cb9b686cc162c395776fbd9d9b57
Sebastián Aedo <saedo@alumnos.inf.utfsm.cl>
2020-04-30 12:40:14 -0400

    first commimt

---
+++ src/main.rs
@@ -0,0 +1,189 @@
+#![feature(naked_functions)]
+#![feature(llvm_asm)]
+use memory_rs::process::process_wrapper::Process;
+use winapi::um::winuser::{GetCursorPos, SetCursorPos, GetAsyncKeyState};
+use winapi::shared::windef::{POINT};
+use std::{thread, time};
+use std::time::{Duration, Instant};
+use std::f32;
+
+#[naked]
+unsafe fn shellcode() {
+    llvm_asm!("
+    push r11
+    lea r11,[rip+0x200-0x9];
+    pushf
+    push rax
+    mov eax, [r11+0x10]
+    test eax, eax
+    pop rax
+    je not_zero
+    movaps xmm4,[r11+0x30]
+    movaps xmm5,[r11]
+    movaps xmm6,[r11+0x20] // 0x220
+    movaps xmm4,[r11+0x30]
+
+not_zero:
+    movaps [r11],xmm5
+    movaps [r11+0x20],xmm6
+    movaps [r11+0x30],xmm4
+
+    popf
+    pop r11
+    movaps [rsp+0x48],xmm4 // adjusted offset of stack pointer + 8
+    ret
+    nop;nop;nop;nop;
+    ": : : : "volatile", "intel");
+}
+
+fn calc_new_focus_point(cam_x: f32, cam_z: f32,
+    cam_y: f32, speed_x: f32, speed_y: f32) -> (f32, f32, f32) {
+
+    // use spherical coordinates to add speed
+    let theta = cam_z.atan2(cam_x) + speed_x;
+
+    let phi = (cam_x.powi(2) + cam_z.powi(2)).sqrt().atan2(cam_y) +
+        speed_y;
+
+    let r = (cam_x.powi(2) + cam_y.powi(2) + cam_z.powi(2)).sqrt();
+
+    let r_cam_x = r*theta.cos()*phi.sin();
+    let r_cam_z = r*theta.sin()*phi.sin();
+    let r_cam_y = r*phi.cos();
+
+    (r_cam_x, r_cam_z, r_cam_y)
+}
+
+
+fn main() {
+    let mut mouse_pos: POINT = POINT::default();
+
+    // latest mouse positions
+    let mut latest_x = 0;
+    let mut latest_y = 0;
+
+    let yakuza = Process::new("Yakuza0.exe");
+
+    let entry_point: usize = 0x18FD38;
+
+    // function that changes the focal length of the cinematics, when
+    // active, nop this
+    let focal_length_f: Vec<u8> = vec![0xFF, 0x90, 0xF0, 0x01, 0x00, 0x00];
+
+    // WIP: Pause the cinematics of the world.
+    let pause_cinematic_original: Vec<u8> = vec![0xE8, 0x43, 0x56, 0x42, 0x00];
+    let mut pause_world = false;
+
+    let p_shellcode = yakuza.inject_shellcode(entry_point, 5,
+        shellcode as usize as *const u8);
+
+
+    let mut active = false;
+
+    let mut restart_mouse = false;
+
+    loop {
+        if (active & restart_mouse) {
+            unsafe { SetCursorPos(1024, 1024) };
+            restart_mouse = !restart_mouse;
+            latest_x = 1024;
+            latest_y = 1024;
+            continue;
+        }
+
+        let start = Instant::now();
+
+        // poll rate
+        thread::sleep(Duration::from_millis(10));
+        unsafe { GetCursorPos(&mut mouse_pos) };
+        let duration = start.elapsed().as_millis() as f32;
+
+        let speed_x = ((mouse_pos.x - latest_x) as f32)/duration/100.;
+        let speed_y = ((mouse_pos.y - latest_y) as f32)/duration/100.;
+
+        // focus position
+        let mut f_cam_x = yakuza.read_value::<f32>(p_shellcode + 0x200);
+        let mut f_cam_y = yakuza.read_value::<f32>(p_shellcode + 0x204);
+        let mut f_cam_z = yakuza.read_value::<f32>(p_shellcode + 0x208);
+
+        // camera position
+        let mut p_cam_x = yakuza.read_value::<f32>(p_shellcode + 0x220);
+        let mut p_cam_y = yakuza.read_value::<f32>(p_shellcode + 0x224);
+        let mut p_cam_z = yakuza.read_value::<f32>(p_shellcode + 0x228);
+
+        // relative camera position
+        let r_cam_x = f_cam_x - p_cam_x;
+        let r_cam_y = f_cam_y - p_cam_y;
+        let r_cam_z = f_cam_z - p_cam_z;
+
+        let mut dp_forward = 0.;
+        let mut dp_sides = 0.;
+
+        unsafe {
+            if (GetAsyncKeyState(0x26) as u32 & 0x8000) != 0 {
+                dp_forward = 0.1;
+            }
+            if (GetAsyncKeyState(0x28) as u32 & 0x8000) != 0 {
+                dp_forward = -0.1;
+            }
+
+            if (GetAsyncKeyState(0x25) as u32 & 0x8000) != 0 {
+                dp_sides = 0.1;
+            }
+            if (GetAsyncKeyState(0x27) as u32 & 0x8000) != 0 {
+                dp_sides = -0.1;
+            }
+        }
+
+        let (r_cam_x, r_cam_z, r_cam_y) = calc_new_focus_point(r_cam_x,
+            r_cam_z, r_cam_y, speed_x, speed_y);
+
+        f_cam_x = p_cam_x + r_cam_x + dp_forward*r_cam_x;
+        f_cam_z = p_cam_z + r_cam_z + dp_forward*r_cam_z;
+        f_cam_y = p_cam_y + r_cam_y + dp_forward*r_cam_y;
+
+        p_cam_x = p_cam_x + dp_forward*r_cam_x;
+        p_cam_z = p_cam_z + dp_forward*r_cam_z;
+        p_cam_y = p_cam_y + dp_forward*r_cam_y;
+
+        yakuza.write_value::<f32>(p_shellcode + 0x200, f_cam_x);
+        yakuza.write_value::<f32>(p_shellcode + 0x204, f_cam_y);
+        yakuza.write_value::<f32>(p_shellcode + 0x208, f_cam_z);
+
+        yakuza.write_value::<f32>(p_shellcode + 0x220, p_cam_x);
+        yakuza.write_value::<f32>(p_shellcode + 0x224, p_cam_y);
+        yakuza.write_value::<f32>(p_shellcode + 0x228, p_cam_z);
+
+        latest_x = mouse_pos.x;
+        latest_y = mouse_pos.y;
+
+        // to scroll infinitely
+        restart_mouse = !restart_mouse;
+        unsafe {
+            if (GetAsyncKeyState(0x13) as u32 & 0x8000) != 0 {
+                active = !active;
+                yakuza.write_value::<u32>(p_shellcode + 0x210, active as u32);
+
+                let c_status = if active { "Deattached" } else { "Attached" };
+                println!("status of camera: {}", c_status);
+
+                if (active) {
+                    yakuza.write_nops(0x1F1840, 6);
+                } else {
+                    yakuza.write_aob(0x1F1840, &focal_length_f);
+                }
+                thread::sleep(Duration::from_millis(500));
+            }
+
+            if (GetAsyncKeyState(0x23) as u32 & 0x8000) != 0 {
+                pause_world = !pause_world;
+                if (pause_world) {
+                    yakuza.write_nops(0x427088, 5);
+                } else {
+                    yakuza.write_aob(0x427088, &pause_cinematic_original);
+                }
+                thread::sleep(Duration::from_millis(500));
+            }
+        }
+    }
+}
