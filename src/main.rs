59585f6676384345cd2a91a603cea9258f7c4d17
Sebastián Aedo <saedo@alumnos.inf.utfsm.cl>
2020-04-30 23:53:32 -0400

    Added icon to release, added horizontal movement, option to deattach the camera

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 #![feature(naked_functions)]
 #![feature(llvm_asm)]
 use memory_rs::process::process_wrapper::Process;
+use winapi::um::winuser;
 use winapi::um::winuser::{GetCursorPos, SetCursorPos, GetAsyncKeyState};
 use winapi::shared::windef::{POINT};
-use std::{thread, time};
+use std::thread;
 use std::time::{Duration, Instant};
 use std::f32;
 
@@ -21,12 +22,11 @@ unsafe fn shellcode() {
     movaps xmm4,[r11+0x30]
     movaps xmm5,[r11]
     movaps xmm6,[r11+0x20] // 0x220
-    movaps xmm4,[r11+0x30]
 
 not_zero:
     movaps [r11],xmm5
     movaps [r11+0x20],xmm6
-    movaps [r11+0x30],xmm4
+    movaps [r11+0x30],xmm4 // camera rotation
 
     popf
     pop r11
@@ -79,11 +79,27 @@ fn main() {
 
 
     let mut active = false;
+    let mut capture_mouse = false;
 
     let mut restart_mouse = false;
 
+    let mut speed_scale = 1.;
+    println!("
+    INSTRUCTIONS:
+
+    PAUSE - Activate/Deactivate Free Camera
+    DEL - Deattach Mouse
+
+    UP, DOWN, LEFT, RIGHT - Move in the direction you're pointing
+    PG UP, PG DOWN - Increase/Decrease speed multiplier
+
+    WARNING: Once you deattach the camera (PAUSE), your mouse will be set in a fixed
+    position, so in order to attach/deattach the mouse to the camera, you can
+    press DEL
+    ");
+
     loop {
-        if (active & restart_mouse) {
+        if (capture_mouse & restart_mouse) {
             unsafe { SetCursorPos(1024, 1024) };
             restart_mouse = !restart_mouse;
             latest_x = 1024;
@@ -120,39 +136,45 @@ fn main() {
         let mut dp_sides = 0.;
 
         unsafe {
-            if (GetAsyncKeyState(0x26) as u32 & 0x8000) != 0 {
-                dp_forward = 0.1;
+            if (GetAsyncKeyState(winuser::VK_UP) as u32 & 0x8000) != 0 {
+                dp_forward = 0.1*speed_scale;
             }
-            if (GetAsyncKeyState(0x28) as u32 & 0x8000) != 0 {
-                dp_forward = -0.1;
+            if (GetAsyncKeyState(winuser::VK_DOWN) as u32 & 0x8000) != 0 {
+                dp_forward = -0.1*speed_scale;
             }
 
-            if (GetAsyncKeyState(0x25) as u32 & 0x8000) != 0 {
-                dp_sides = 0.1;
+            if (GetAsyncKeyState(winuser::VK_LEFT) as u32 & 0x8000) != 0 {
+                dp_sides = 0.1*speed_scale;
             }
-            if (GetAsyncKeyState(0x27) as u32 & 0x8000) != 0 {
-                dp_sides = -0.1;
+            if (GetAsyncKeyState(winuser::VK_RIGHT) as u32 & 0x8000) != 0 {
+                dp_sides = -0.1*speed_scale;
             }
         }
 
         let (r_cam_x, r_cam_z, r_cam_y) = calc_new_focus_point(r_cam_x,
             r_cam_z, r_cam_y, speed_x, speed_y);
 
-        f_cam_x = p_cam_x + r_cam_x + dp_forward*r_cam_x;
-        f_cam_z = p_cam_z + r_cam_z + dp_forward*r_cam_z;
+        f_cam_x = p_cam_x + r_cam_x + dp_forward*r_cam_x + dp_sides*r_cam_z;
+        f_cam_z = p_cam_z + r_cam_z + dp_forward*r_cam_z - dp_sides*r_cam_x;
         f_cam_y = p_cam_y + r_cam_y + dp_forward*r_cam_y;
 
-        p_cam_x = p_cam_x + dp_forward*r_cam_x;
-        p_cam_z = p_cam_z + dp_forward*r_cam_z;
+        p_cam_x = p_cam_x + dp_forward*r_cam_x + dp_sides*r_cam_z;
+        p_cam_z = p_cam_z + dp_forward*r_cam_z - dp_sides*r_cam_x;
         p_cam_y = p_cam_y + dp_forward*r_cam_y;
 
-        yakuza.write_value::<f32>(p_shellcode + 0x200, f_cam_x);
-        yakuza.write_value::<f32>(p_shellcode + 0x204, f_cam_y);
-        yakuza.write_value::<f32>(p_shellcode + 0x208, f_cam_z);
+        if capture_mouse {
+            yakuza.write_value::<f32>(p_shellcode + 0x200, f_cam_x);
+            yakuza.write_value::<f32>(p_shellcode + 0x204, f_cam_y);
+            yakuza.write_value::<f32>(p_shellcode + 0x208, f_cam_z);
 
-        yakuza.write_value::<f32>(p_shellcode + 0x220, p_cam_x);
-        yakuza.write_value::<f32>(p_shellcode + 0x224, p_cam_y);
-        yakuza.write_value::<f32>(p_shellcode + 0x228, p_cam_z);
+            yakuza.write_value::<f32>(p_shellcode + 0x220, p_cam_x);
+            yakuza.write_value::<f32>(p_shellcode + 0x224, p_cam_y);
+            yakuza.write_value::<f32>(p_shellcode + 0x228, p_cam_z);
+
+            yakuza.write_value::<f32>(p_shellcode + 0x230, 0.);
+            yakuza.write_value::<f32>(p_shellcode + 0x234, 1.);
+            yakuza.write_value::<f32>(p_shellcode + 0x238, 0.);
+        }
 
         latest_x = mouse_pos.x;
         latest_y = mouse_pos.y;
@@ -160,14 +182,15 @@ fn main() {
         // to scroll infinitely
         restart_mouse = !restart_mouse;
         unsafe {
-            if (GetAsyncKeyState(0x13) as u32 & 0x8000) != 0 {
+            if (GetAsyncKeyState(winuser::VK_PAUSE) as u32 & 0x8000) != 0 {
                 active = !active;
+                capture_mouse = active;
                 yakuza.write_value::<u32>(p_shellcode + 0x210, active as u32);
 
                 let c_status = if active { "Deattached" } else { "Attached" };
                 println!("status of camera: {}", c_status);
 
-                if (active) {
+                if active {
                     yakuza.write_nops(0x1F1840, 6);
                 } else {
                     yakuza.write_aob(0x1F1840, &focal_length_f);
@@ -175,9 +198,30 @@ fn main() {
                 thread::sleep(Duration::from_millis(500));
             }
 
-            if (GetAsyncKeyState(0x23) as u32 & 0x8000) != 0 {
+            if GetAsyncKeyState(winuser::VK_DELETE) as u32 & 0x8000 != 0 {
+                capture_mouse = !capture_mouse;
+            }
+
+            if (GetAsyncKeyState(winuser::VK_PRIOR) as u32 & 0x8000) != 0 {
+                speed_scale += 0.1;
+                println!("Speed increased, {:.2}", speed_scale);
+                thread::sleep(Duration::from_millis(100));
+            }
+
+            if (GetAsyncKeyState(winuser::VK_NEXT) as u32 & 0x8000) != 0 {
+                if speed_scale > 0.1 {
+                    speed_scale -= 0.1;
+                    println!("Speed decreased, {:.2}", speed_scale);
+                } else {
+                    println!("Cannot be decreased, {:.2}", speed_scale);
+                }
+                thread::sleep(Duration::from_millis(100));
+            }
+
+            if (GetAsyncKeyState(winuser::VK_F2) as u32 & 0x8000) != 0 {
                 pause_world = !pause_world;
-                if (pause_world) {
+                println!("status of pausing: {}", pause_world);
+                if pause_world {
                     yakuza.write_nops(0x427088, 5);
                 } else {
                     yakuza.write_aob(0x427088, &pause_cinematic_original);
