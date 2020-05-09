795bbcba5996748176c5acebc0a30b2a0284a467
Jessica Stokes <hello@jessicastokes.net>
2020-05-09 13:27:32 -0700

    Add a specific error message for when the game is not running
    This takes advantage of the updates in memory-rs which return Result types to provide a more user-friendly message if finding the running game fails.
    
    Firstly, it updates main.rs to only compile in the module for the specific game, and then to print the error and exit with an error code.
    
    Secondly, both the zero and kiwami2 modules have been updated to return a Result, and to map the OS error 18 (ERROR_NO_MORE_FILES) to a helpful error message about the game not currently running.
    
    Now, instead of a panic, you get "Error: Yakuza 0 needs to be running"!

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,25 @@
 #![feature(naked_functions)]
 #![feature(llvm_asm)]
-mod zero;
+
+#[cfg(feature = "kiwami2")]
 mod kiwami2;
 
+#[cfg(not(feature = "kiwami2"))]
+mod zero;
+
 fn main() {
-    if cfg!(feature = "kiwami2") {
-        kiwami2::main();
-    } else {
-        zero::main();
-    }
+    #[cfg(feature = "kiwami2")]
+    let result = kiwami2::main();
+
+    #[cfg(not(feature = "kiwami2"))]
+    let result = zero::main();
+
+    // Slightly nicer way to print exit codes
+    std::process::exit(match result {
+        Ok(_) => 0,
+        Err(error) => {
+            eprintln!("Error: {}", error);
+            1
+        }
+    })
 }
