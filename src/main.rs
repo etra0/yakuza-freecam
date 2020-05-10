4c98885221b9719d7718de0de61c17f1f6ac1263
Jessica Stokes <hello@jessicastokes.net>
2020-05-09 18:25:43 -0700

    If launched outside cmd.exe, wait for input before exiting after errors
    This became a bit of a sidetrack for me... 😅
    
    I'm not much of a Windows developer, but I wondered if there was a way to make this not immediately exit when opened from Firefox or Explorer. Eventually I found the `dont_disappear` crate, but it bugged me that it didn't care _where_ the program was launched from.
    
    I ended up wrapping it with a function intended to do its best to determine whether the parent process was `cmd.exe` or `cargo`, and if it's neither it will wait for input. This introduces a few new dependencies, but compiled with msvc the exe still seems pretty small, and I've done my best to ensure this ignores any errors produced while trying to determine this.
    
    Totally understandable if this isn't something you want, but it feels a bit nicer to me, and either way was a nice exercise in figuring out some Windows stuff for me 😇

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,62 @@
 #![feature(naked_functions)]
 #![feature(llvm_asm)]
+use sysinfo::{ProcessExt, RefreshKind, SystemExt};
 
 #[cfg(feature = "kiwami2")]
 mod kiwami2;
 
 #[cfg(not(feature = "kiwami2"))]
 mod zero;
+
+fn should_wait_before_exiting() -> bool {
+    let current_pid = match sysinfo::get_current_pid() {
+        Ok(pid) => pid,
+        Err(_) => return false,
+    };
+
+    let system = sysinfo::System::new_with_specifics(RefreshKind::new().with_processes());
+
+    let current_process = match system.get_process(current_pid) {
+        Some(process) => process,
+        None => return false,
+    };
+
+    let parent_pid = match current_process.parent() {
+        Some(ppid) => ppid,
+        None => return false,
+    };
+
+    let parent_process = match system.get_process(parent_pid) {
+        Some(parent) => parent,
+        None => return false,
+    };
+
+    let parent_exe = match parent_process.exe().canonicalize() {
+        Ok(exe) => exe,
+        Err(_) => return false,
+    };
+
+    if parent_exe.ends_with("cargo.exe") {
+    	return false
+    }
+
+    let system_directory = unsafe {
+        let mut buffer: [u16; 255] = std::mem::zeroed();
+        let read_len =
+            winapi::um::sysinfoapi::GetSystemDirectoryW(buffer.as_mut_ptr(), buffer.len() as u32);
+        String::from_utf16_lossy(&buffer[0..(read_len as usize)])
+    };
+
+    let cmd_exe = match std::path::Path::new(&system_directory)
+        .join("cmd.exe")
+        .canonicalize()
+    {
+        Ok(cmd) => cmd,
+        Err(_) => return false,
+    };
+
+    return parent_exe != cmd_exe;
+}
 
 fn main() {
     #[cfg(feature = "kiwami2")]
@@ -19,6 +70,12 @@ fn main() {
         Ok(_) => 0,
         Err(error) => {
             eprintln!("Error: {}", error);
+
+            // Be extra super nice and wait if we're not run from command-line
+            if should_wait_before_exiting() {
+                dont_disappear::any_key_to_continue::default();
+            }
+
             1
         }
     })
