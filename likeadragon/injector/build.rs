35f3553c6d5bf94699a9ae29972c3304de6c4248
Sebastián Aedo <sebastian.aedo@sansano.usm.cl>
2020-12-01 18:22:33 -0300

    Major project reestructuration of ylad

---
+++ likeadragon/injector/build.rs
@@ -0,0 +1,10 @@
+extern crate winres;
+
+fn main() {
+    let mut res = winres::WindowsResource::new();
+
+    res.set_icon("../../assets/likeadragon.ico")
+        .set("InternalName", "likeadragon-freecam");
+
+    res.compile().unwrap();
+}
